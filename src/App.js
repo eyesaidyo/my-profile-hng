e79b3e1fcebd4d81425d4e7d674cf49564f5dec2
John Rasine Irem <rajonirem8@gmail.com>
2022-10-27 18:51:06 +0100

    added necessary elements with minimal styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import slack from "./slack.svg";
+import github from "./github.svg";
+import i4g from "./i4g.svg";
+import zuri from "./zuri.svg";
+import "./App.css";
+import Card from "./Card";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <img
+        alt="profile"
+        src="https://pbs.twimg.com/media/FFHy-ipXIAITdC4.jpg"
+        id="profile__img"
+      />
+      <h3>John Irem</h3>
+      <h3 id="slack">Rajon</h3>
+      <div className="cards-container">
+        <Card
+          link="https://training.zuri.team/"
+          content="Zuri Team"
+          Id="btn__zuri"
+        />
+        <Card link="https://books.zuri.team/" content="Zuri Books" Id="books" />
+        <Card
+          link="https://books.zuri.team/"
+          content="Python Books"
+          Id="books"
+        />
+        <Card
+          link="https://background.zuri.team/"
+          content="Background checks for coders"
+          Id="pitch"
+        />
+        <Card />
+        <Card />
+        <img src={slack} alt="slack" />
+        <a href="https://github.com/eyesaidyo">
+          <img src={github} alt="github" />
         </a>
-      </header>
+        <footer>
+          <img src={zuri} alt="zuri" />
+          <p>HNG Internship 9</p>
+          <img src={i4g} alt="i4g" />
+        </footer>
+      </div>
     </div>
   );
 }
